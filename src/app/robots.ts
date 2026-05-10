887b173545f543e8bbc46e41f8d4b6e38b40d462
Devraj069 <devraj1625r@gmail.com>
2026-05-10 15:46:23 +0530

    fix: update sitemap and robots domains for deployment

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/_next/'],
     },
-    sitemap: 'https://rewardverse.in/sitemap.xml',
+    sitemap: 'https://rewardverse.satyainfotechnetworks.com/sitemap.xml',
   }
 }
